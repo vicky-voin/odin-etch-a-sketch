0bc8a506486702eefc2d53dba613b13b7e0be10c
vicky-voin <alteneria127@gmail.com>
2025-05-22 20:30:48 -0400

    Set up filling in for the cells

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const gridContainer = document.querySelector(".gridContainer");
 
-for(let i = 0; i < 16; i++)
-{
+for (let i = 0; i < 16; i++) {
     const cell = document.createElement("div");
     cell.className = "cell";
+    cell.addEventListener("mouseenter", function (e) {
+        e.target.classList.add("filled");
+    });
+
     gridContainer.appendChild(cell);
 }
