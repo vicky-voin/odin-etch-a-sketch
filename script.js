e73e812ed691443fbf6c6fe83aa9df67a1ae5106
vicky-voin <alteneria127@gmail.com>
2025-05-22 20:44:05 -0400

    Refactored to allow for grids of different sizes

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const gridContainer = document.querySelector(".gridContainer");
 
-for (let i = 0; i < 16; i++) {
-    const cell = document.createElement("div");
-    cell.className = "cell";
-    cell.addEventListener("mouseenter", function (e) {
-        e.target.classList.add("filled");
-    });
+generateGrid(4);
 
-    gridContainer.appendChild(cell);
+function generateGrid(dimension)
+{
+    const gridWidth = gridContainer.offsetWidth;
+
+    for (let i = 0; i < dimension * dimension; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.style.width = gridWidth / dimension - cell.style.borderWidth;
+        cell.addEventListener("mouseenter", function (e) {
+            e.target.classList.add("filled");
+        });
+    
+        gridContainer.appendChild(cell);
+    }
 }
